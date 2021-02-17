8ab136dc7f8cb56bb520893694da6018de77fc65
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-02-17 19:16:17 +0800

    Add files via upload

---
+++ YoutubeChannel_나동빈/Tutorial5/ifstatement.java
@@ -0,0 +1,20 @@
+
+public class ifstatement {
+
+	public static void main(String[] args) {
+		
+		String a = "I LOVE YOU";
+		
+		if(a.contains("LOVE") == true)
+			// ==true 는 생략해도 가넝~~  
+		{
+			System.out.println("It contains the word 'LOVE'");
+		}
+		
+		else
+		{
+			System.out.println("It does not contain the word 'LOVE'");
+		}
+	}
+
+}
