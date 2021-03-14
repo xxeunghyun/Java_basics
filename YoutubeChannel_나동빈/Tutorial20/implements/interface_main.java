60419ab4ddb5155de7c806189e43b1c88c99f8fa
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-03-14 22:51:31 +0800

    Add files via upload

---
+++ YoutubeChannel_나동빈/Tutorial20/implements/interface_main.java
@@ -0,0 +1,29 @@
+
+public class interface_main implements Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		interface_main main = new interface_main();
+		main.crying();
+		main.show();
+		
+		
+		
+
+	}
+
+	@Override
+	public void crying() {
+		System.out.println("RHRFHRGHHR");
+		
+	}
+
+	@Override
+	public void show() {
+		System.out.println("Hello World!");
+
+	}
+
+	
+}
