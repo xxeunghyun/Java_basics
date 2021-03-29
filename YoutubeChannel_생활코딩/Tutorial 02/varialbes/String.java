546798f402d5e3ca0647a9d66dc18c07f4e9a4cc
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-03-29 21:25:15 +0800

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 02/varialbes/String.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		
+		java.lang.String a;
+		java.lang.String b;
+		a = "coding";
+		b = "everybody";
+		
+		System.out.println(a+b);
+		
+
+	}
+
+}
