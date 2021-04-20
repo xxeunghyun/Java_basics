a7b593fc9e0427e9eea5bf334d74a2d1302d2695
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-04-20 23:52:20 +0800

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial03/src/IntDatatype/IntDatatypeDemo.java
@@ -0,0 +1,23 @@
+package IntDatatype;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte a;
+		a = 127; // within range > no problem
+		//a = 128; // out of range > got problem
+
+	}
+
+}
+
+
+/*
+ * byte = -128~127
+ * short = 2 byte = -32768~32767
+ * int = 4 byte
+ * long = 8 byte
+ */
+ 
