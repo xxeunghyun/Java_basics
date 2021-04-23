3b087d7507685bd21df5d359cd28ffc459924712
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-04-23 14:36:36 +0800

    Add files via upload

---
+++ Tutorial 02/varialbes/IntegerDemo.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		int a;
+		a = 1;
+		System.out.println(a+1); //2
+		
+		a = 2;
+		System.out.println(a+1); //3
+
+	}
+
+}
