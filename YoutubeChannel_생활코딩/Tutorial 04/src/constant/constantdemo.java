48a2a16508d79cf0ca3c92ccfb68973056e726d7
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-04-28 17:56:02 +0800

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 04/src/constant/constantdemo.java
@@ -0,0 +1,21 @@
+/*
+ * 상수형 데이터타입  
+ */
+
+
+package constant;
+
+public class constantdemo {
+
+	public static void main(String[] args) {
+		
+		int a = 20;	
+		double b = 2.2;
+		float c = 2.2f;
+		long d = 23453452L;		
+		byte e = 100;
+		
+
+	}
+
+}
