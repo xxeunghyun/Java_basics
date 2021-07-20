e6d443ff7cba89d4af791732b77fde35d321e653
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-07-20 19:44:05 +0900

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 06/src/compare/equal.java
@@ -0,0 +1,25 @@
+package compare;
+
+public class equal {
+
+	public static void main(String[] args) {
+		System.out.println(1==2); 
+		System.out.println(1==1); 
+		System.out.println(1!=2); 
+		
+		System.out.println();
+		
+		System.out.println("one" == "two"); 
+		System.out.println("one" == "one");
+		System.out.println("one" != "two"); 
+
+		System.out.println(); 
+
+		boolean a = true;
+		System.out.println(a); 
+		System.out.println(!a);
+
+
+	}
+
+}
