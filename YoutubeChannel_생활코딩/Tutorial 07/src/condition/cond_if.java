7498c17c7574f89bdc6ac1d3aa02b2be2ba8bfb0
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-07-29 20:09:35 +0900

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 07/src/condition/cond_if.java
@@ -0,0 +1,34 @@
+package condition;
+
+public class cond_if {
+
+	public static void main(String[] args) {
+	/*	if(true)
+		{
+			System.out.println("result : true");
+		}
+		*/
+		System.out.println("when the value is true");
+		if(true)
+		{
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+		}
+		System.out.println(5);
+	
+
+	
+	System.out.println("\n\nwhen the value is false");
+	if(false)
+	{
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+	System.out.println(5);
+}
+	
+}
