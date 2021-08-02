fc866cadaf47110e5e023c6aad4cadb7b149f561
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-08-03 00:36:35 +0900

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 07/src/condition/cond_ifelse.java
@@ -0,0 +1,19 @@
+package condition;
+
+public class cond_ifelse {
+
+	public static void main(String[] args) {
+		
+		if (!true)
+		{
+			System.out.println(1);
+		}
+		
+		else
+		{
+			System.out.println(2);
+		}
+
+	}
+
+}
