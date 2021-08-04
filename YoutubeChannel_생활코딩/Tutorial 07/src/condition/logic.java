41351000fd9fd3d6b99fc1bd60ca667823b6d61a
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-08-04 23:30:56 +0900

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 07/src/condition/logic.java
@@ -0,0 +1,21 @@
+package condition;
+
+public class logic {
+
+	public static void main(String[] args) {
+		String id = args[0];
+		
+		if(id.equals("egoing"))
+		{
+			System.out.println("right");
+			
+		}
+		
+		else
+		{
+			System.out.println("wrong");
+		}
+
+	}
+
+}
