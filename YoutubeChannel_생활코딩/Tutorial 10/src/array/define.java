0523f397117227c681adfcfc07c9058131a991fe
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-08-13 15:43:59 +0800

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 10/src/array/define.java
@@ -0,0 +1,15 @@
+package array;
+
+public class define {
+
+	public static void main(String[] args) {
+		String [] classgroup = {"최진혁","최유빈","한이람","이고잉"};
+		
+		for(int i=0;i<4;i++)
+		{
+			System.out.println(classgroup[i]);
+		}
+
+	}
+
+}
