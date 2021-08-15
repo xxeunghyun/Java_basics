88681720bb78f1e6302b707e7c89581ad2ef709c
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-08-15 22:31:31 +0800

    Add files via upload

---
+++ YoutubeChannel_생활코딩/Tutorial 10/src/array/foreach.java
@@ -0,0 +1,14 @@
+package array;
+
+public class foreach {
+
+	public static void main(String[] args) {
+		String[] member = {"최진혁", "최이람", "한유빈"};
+		for(String i : member)
+		{
+			System.out.println(i+"이 상담을 완료하였습니다. ");
+		}
+
+	}
+
+}
